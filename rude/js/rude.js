04817571237f92a5df07922285fc0a62d97043e5
vicchi <gary@vicchi.org>
2013-02-06 12:47:45 +0000

    First commit

---
+++ rude/js/rude.js
@@ -0,0 +1,858 @@
+var places = [
+	{
+		label: 'Anus, Batangas, Philippines',
+		detail: 'Brgy. Anus Road, Cuenca, Philippines',
+		lat: 13.86861,
+		lon: 121.0724638
+	},
+	{
+		label: 'Anus, Irian Jaya, Indonesia',
+		detail: 'Pulau Anus, Indonesia',
+		lat: -2.1258537,
+		lon: 139.514001
+	},
+	{
+		label: 'Anus, Laguna, Philippines',
+		detail: 'Philippines',
+		lat: 12.879721,
+		lon: 121.774017
+	},
+	{
+		label: 'Arsoli, Lazio, Italy',
+		detail: '00023 Arsoli Province of Rome, Italy',
+		lat: 42.0417472,
+		lon: 13.0152124
+	},
+	{
+		label: 'Bald Knob, Arkansas, United States',
+		detail: 'Bald Knob, AR, USA',
+		lat: 35.3098066,
+		lon: -91.5679114
+	},
+	{
+		label: 'Balls Cross, West Sussex, United Kingdom',
+		detail: 'Balls Cross, West Sussex GU28, UK',
+		lat: 51.027109,
+		lon: -0.592081
+	},
+	{
+		label: 'Beaver, Oklahoma, United States',
+		detail: 'Beaver, OK 73932, USA',
+		lat: 36.8161371,
+		lon: -100.5198713
+	},
+	{
+		label: 'Beaver, West Virginia, United States',
+		detail: 'Beaver, WV, USA',
+		lat: 37.7476157,
+		lon: -81.1423203
+	},
+	{
+		label: 'Beaver Crossing, Nebraska, United States',
+		detail: 'Beaver Crossing, NE 68313, USA',
+		lat: 40.7786191,
+		lon: -97.2822684
+	},
+	{
+		label: 'Beaver Head, Idaho, United States',
+		detail: 'Beaverhead, MT, USA',
+		lat: 45.2135118,
+		lon: -113.1093046
+	},
+	{
+		label: 'Bell End, Worcestershire, United Kingdom',
+		detail: 'Bell End, Worcestershire DY9, UK',
+		lat: 52.3921,
+		lon: -2.086302
+	},
+	{
+		label: 'Big Beaver, Pennsylvania, United States',
+		detail: 'Big Beaver, PA, USA',
+		lat: 40.8245081,
+		lon: -80.362842
+	},
+	{
+		label: 'Brown Willy, Cornwall, United Kingdom',
+		detail: 'Brown Willy, Camelford, Cornwall PL32, UK',
+		lat: 50.5905833,
+		lon: -4.6027499
+	},
+	{
+		label: 'BUM, Bates, Missouri, United States',
+		detail: 'Butler Airport (BUM), Butler, MO 64730, USA',
+		lat: 38.289722,
+		lon: -94.340279
+	},
+	{
+		label: 'Buger, Majorca, Spain',
+		detail: '07311 Búger, Balearic Islands, Spain',
+		lat: 39.7585631,
+		lon: 2.9844311
+	},
+	{
+		label: 'Bum, Southern, Sierra Leone',
+		detail: 'Bum, Sierra Leone',
+		lat: 7.5,
+		lon: -11.883333
+	},
+	{
+		label: 'Bumbang, Victoria, Australia',
+		detail: 'Bumbang Island Historical Reserve, Robinvale VIC 3549, Australia',
+		lat: -34.5639568,
+		lon: 142.7910944
+	},
+	{
+		label: 'Chinaman\'s Knob, Australia',
+		detail: 'Chinamans Knob, The Branch NSW 2425, Australia',
+		lat: -32.5666667,
+		lon: 152.0333333
+	},
+	{
+		label: 'Clam, Accomack, Virginia, United States',
+		detail: 'Clam, 4, VA 23308, USA',
+		lat: 37.8137386,
+		lon: -75.670207
+	},
+	{
+		label: 'Clam, Charente-Maritime, Poitou-Charentes, France',
+		detail: 'Clam, France',
+		lat: 45.4916709,
+		lon: -0.448916
+	},
+	{
+		label: 'Climax, Colorado, United States',
+		detail: 'Climax, CO 80461, USA',
+		lat: 39.37635,
+		lon: -106.185173
+	},
+	{
+		label: 'Climax, Polk County, Minnesota, United States',
+		detail: 'Climax, MN 56523, USA',
+		lat: 47.6077515,
+		lon: -96.8170222
+	},
+	{
+		label: 'Climax, Saskatchewan, Canada',
+		detail: 'Climax, SK S0N, Canada',
+		lat: 49.2081809,
+		lon: -108.3895518
+	},
+	{
+		label: 'Clit, Arad, Romania',
+		detail: 'Clit, Romania',
+		lat: 46.5149386,
+		lon: 22.0923374
+	},
+	{
+		label: 'Clit, Salja, Romania',
+		detail: 'Băbeni, Romania',
+		lat: 47.3025,
+		lon: 23.4011
+	},
+	{
+		label: 'Clit, Suceava, Romania',
+		detail: 'Clit, Romania',
+		lat: 47.7533717,
+		lon: 25.8496702
+	},
+	{
+		label: 'Conception Bay, Newfoundland',
+		detail: 'Conception Bay, Division No. 1, Subd. G, NL, Canada',
+		lat: 47.8642331,
+		lon: -53.0346963
+	},
+	{
+		label: 'The Cock Of Arran, Scotland, United Kingdom',
+		detail: 'Cock of Arran, United Kingdom',
+		lat: 55.7333333,
+		lon: -5.25
+	},
+	{
+		label: 'Cock Bridge, Aberdeen, Scotland, United Kingdom',
+		detail: 'Cock Bridge, Aberdeenshire AB36, UK',
+		lat: 57.1656217,
+		lon: -3.2306047
+	},
+	{
+		label: 'Cockermouth, North (Cumbria, Allerdale), United Kingdom',
+		detail: 'Cockermouth, Cumbria CA13, UK',
+		lat: 54.663261,
+		lon: -3.367985
+	},
+	{
+		label: 'Cocks, Perranporth, Cornwall, United Kingdom',
+		detail: 'Cocks Hill, Perranporth, Cornwall TR6, UK',
+		lat: 50.3311644,
+		lon: -5.1384796
+	},
+	{
+		label: 'Condom, Aveyron, Midi-Pyrénées, France',
+		detail: 'Condom, 12470 Condom-d\'Aubrac, France',
+		lat: 44.605101,
+		lon: 2.863612
+	},
+	{
+		label: 'Crap, Tirane, Albania',
+		detail: 'Stacioni i 3-shit, SH1, Tirana, Albania',
+		lat: 41.3514399,
+		lon: 19.7708201
+	},
+	{
+		label: 'Crotch Lake, Ontario, Canada',
+		detail: 'Crotch Lake, Elliot Lake, ON P0R, Canada',
+		lat: 46.4252196,
+		lon: -82.5996091
+	},
+	{
+		label: 'Cuckoo\'s Knob, Wiltshire, UK',
+		detail: 'Cuckoo\'s Knob, Wootton Rivers, Marlborough, Wiltshire SN8 4NR, UK',
+		lat: 51.359593,
+		lon: -1.7182098
+	},
+	{
+		label: 'Cum, Wanderlandia, Tocantins, Brazil',
+		detail: 'Cum, Wanderlândia - Tocantins, Brazil',
+		lat: -6.922388,
+		lon: -48.11956
+	},
+	{
+		label: 'Cumbum, Andhra Pradesh, India',
+		detail: 'Cumbum, Andhra Pradesh, India',
+		lat: 15.581308,
+		lon: 79.110646
+	},
+	{
+		label: 'Cumming, Georgia, United States',
+		detail: 'Cumming, GA, USA',
+		lat: 34.2073196,
+		lon: -84.1401926
+	},
+	{
+		label: 'Cunter, Switzerland',
+		detail: 'Cunter, Switzerland',
+		lat: 46.6108649,
+		lon: 9.592852
+	},
+	{
+		label: 'Dick, Weld County, Colorado, United States',
+		detail: 'Dick, Colorado 80603, USA',
+		lat: 40.0313729,
+		lon: -104.9371996
+	},
+	{
+		label: 'Dick, Chippewa County, Michigan, United States',
+		detail: 'Dick, Hiawatha National Forest, Trout Lake, MI 49780, USA',
+		lat: 46.1908484,
+		lon: -84.8314496
+	},
+	{
+		label: 'Dick, Westmoreland County, Pennsylvania, United States',
+		detail: 'Dick, Sewickley, PA 15642, USA',
+		lat: 40.2581257,
+		lon: -79.7353216
+	},
+	{
+		label: 'Dikshit, India',
+		detail: 'V S Dikshit, Girgaon, Mumbai, Maharashtra 400004, India',
+		lat: 18.9519756,
+		lon: 72.8189519
+	},
+	{
+		label: 'Dildo, Trinity Bay, Newfoundland',
+		detail: 'The Salvation Army Trinity Bay South Corps, Newfoundland and Labrador 80, Dildo, NL A0B 1P0, Canada',
+		lat: 47.5753541,
+		lon: -53.5501881
+	},
+	{
+		label: 'Dong, CHUXIONG, Yunan, China',
+		detail: 'East Street, Nanhua, Chuxiong, Yunnan, China',
+		lat: 25.1910581,
+		lon: 101.2747388
+	},
+	{
+		label: 'Dong, Khon Kaen, Thailand',
+		detail: 'Pa Dong Cham National Park, Huai Toei, Sam Sung, Khon Kaen 40170, Thailand',
+		lat: 16.608909,
+		lon: 103.084472
+	},
+	{
+		label: 'Dong, Lohit, Arunachal Pradesh, India',
+		detail: 'Arunachal Pradesh',
+		lat: 28.2390993,
+		lon: 94.0695454
+	},
+	{
+		label: 'Dong Rak, Thailand',
+		detail: 'Dong Rak, Phu Sing, Si Sa Ket 33140, Thailand',
+		lat: 14.5049148,
+		lon: 104.1986514
+	},
+	{
+		label: 'Dongo, Democratic Republic of Congo',
+		detail: 'Dongo, Democratic Republic of the Congo',
+		lat: 2.7188299,
+		lon: 18.4068343
+	},
+	{
+		label: 'Dripsey, County Cork, Republic of Ireland',
+		detail: 'Dripsey, Co. Cork, Ireland',
+		lat: 51.9143772,
+		lon: -8.7420906
+	},
+	{
+		label: 'Dump Ln, Yallahs, St Thomas Parish, Jamaica',
+		detail: 'Yallahs, Jamaica',
+		lat: 17.8768385,
+		lon: -76.5517132
+	},
+	{
+		label: 'Effin, Limerick, Republic of Ireland',
+		detail: 'Effin, Co. Limerick, Ireland',
+		lat: 52.3652853,
+		lon: -8.6301216
+	},
+	{
+		label: 'Erect, Randolph County, North Carolina, United States',
+		detail: 'Erect, NC 27316, USA',
+		lat: 35.6289942,
+		lon: -79.6449759
+	},
+	{
+		label: 'Fanny, West Virginia (Wyoming), United States',
+		detail: 'Fanny, WV 24818, USA',
+		lat: 37.5658109,
+		lon: -81.631653
+	},
+	{
+		label: 'Felch, Michigan (Dickinson), United States',
+		detail: 'Felch, MI 49831, USA',
+		lat: 45.997222,
+		lon: -87.825556
+	},
+	{
+		label: 'Felchville, Vermont, United States',
+		detail: 'Felchville, Reading, VT 05062, USA',
+		lat: 43.4564604,
+		lon: -72.53787
+	},
+	{
+		label: 'Felchville, MA, United States',
+		detail: 'Felchville, Natick, MA 01760, USA',
+		lat: 42.297874,
+		lon: -71.3592258
+	},
+	{
+		label: 'Fertile, Polk County, Minnesota, United State',
+		detail: 'Fertile, MN 56540, USA',
+		lat: 47.5360765,
+		lon: -96.2803328
+	},
+	{
+		label: 'Finger, Cabarrus, North Carolina, United States',
+		detail: 'Finger, Mount Pleasant, NC 28124, USA',
+		lat: 35.384864,
+		lon: -80.357839
+	},
+	{
+		label: 'Finger, McNairy, Tennessee, United States',
+		detail: 'Finger, TN 38334, USA',
+		lat: 35.3584121,
+		lon: -88.5989385
+	},
+	{
+		label: 'French Lick, Indiana, United States',
+		detail: 'French Lick, IN, USA',
+		lat: 38.5489431,
+		lon: -86.6199914
+	},
+	{
+		label: 'Fist, Lezhe, Albania',
+		detail: 'E 762, Lezhë District, Albania',
+		lat: 41.7901717,
+		lon: 19.6477917
+	},
+	{
+		label: 'Fucking, Tarsdorf, Austria',
+		detail: '5121 Fucking, Austria',
+		lat: 48.06733,
+		lon: 12.86327
+	},
+	{
+		label: 'Fuku, Shensi, China',
+		detail: 'Shaanxi, China',
+		lat: 34.265472,
+		lon: 108.954239
+	},
+	{
+		label: 'Fukue, Nagoya, Aichi Prefecture, Japan',
+		detail: 'Fukue, Showa Ward, Nagoya, Aichi Prefecture, Japan',
+		lat: 35.1479429,
+		lon: 136.9123816
+	},
+	{
+		label: 'Gaylord, Michigan, United States',
+		detail: 'Gaylord, MI 49735, USA',
+		lat: 45.0275126,
+		lon: -84.6747523
+	},
+	{
+		label: 'Gayville, South Dakota, United States',
+		detail: 'Gayville, SD 57031, USA',
+		lat: 42.8880517,
+		lon: -97.1722732
+	},
+	{
+		label: 'Gland, Switzerland',
+		detail: 'Gland, Switzerland',
+		lat: 46.4206407,
+		lon: 6.271175
+	},
+	{
+		label: 'Hooker, Oklahoma',
+		detail: 'Hooker, OK 73945, USA',
+		lat: 36.8600261,
+		lon: -101.2134954
+	},
+	{
+		label: 'Horn, Horn, Niederosterreich, Austria',
+		detail: 'Horn, Austria',
+		lat: 48.66607,
+		lon: 15.65716
+	},
+	{
+		label: 'Horn, Iceland',
+		detail: 'Horn, Iceland',
+		lat: 66.447548,
+		lon: -22.438589
+	},
+	{
+		label: 'Horn, Brønnøy, Nordland, Norway',
+		detail: 'Horn, Norway',
+		lat: 65.576973,
+		lon: 12.31491
+	},
+	{
+		label: 'Horneytown, North Carolina, USA',
+		detail: 'Horneytown, Kernersville, NC 27284, USA',
+		lat: 36.0195814,
+		lon: -80.0600417
+	},
+	{
+		label: 'Humptulips, Washington, USA',
+		detail: 'Humptulips, WA, USA',
+		lat: 47.2328651,
+		lon: -123.9593406
+	},
+	{
+		label: 'Intercourse, Pennsylvania, USA',
+		detail: 'Intercourse, PA, USA',
+		lat: 40.0383787,
+		lon: -76.1075043
+	},
+	{
+		label: 'Knob Lick, Kentucky, USA',
+		detail: 'Knob Lick, KY 42154, USA',
+		lat: 37.078333,
+		lon: -85.694444
+	},
+	{
+		label: 'Labia, Waremme, Liege, Belgium',
+		detail: 'Rue de Labia, 4317 Faimes, Belgium',
+		lat: 50.6594105,
+		lon: 5.2424133
+	},
+	{
+		label: 'Lickey End, West Midlands, United Kingdom',
+		detail: 'Lickey End, Worcestershire, UK',
+		lat: 52.353096,
+		lon: -2.0350039
+	},
+	{
+		label: 'Little Dix Village, West Indies',
+		detail: 'Little Dix, Anguilla',
+		lat: 18.2325129,
+		lon: -63.037202
+	},
+	{
+		label: 'Meat Camp, North Carolina, United States',
+		detail: 'Meat Camp, NC 28684, USA',
+		lat: 36.299571,
+		lon: -81.6764977
+	},
+	{
+		label: 'Meat Cove, Nova Scotia, Canada',
+		detail: 'Meat Cove, NS B0E, Canada',
+		lat: 47.026539,
+		lon: -60.559561
+	},
+	{
+		label: 'Mianus, Conneticut, United States',
+		detail: 'Mianus, Cos Cob, CT 06807, USA',
+		lat: 41.0439859,
+		lon: -73.5912362
+	},
+	{
+		label: 'Middle Intercourse Island, Australia',
+		detail: 'East Middle Intercourse Island, Western Australia, Australia',
+		lat: -20.6666667,
+		lon: 116.6833333
+	},
+	{
+		label: 'Mingė, Klaipėda County, Lithuania',
+		detail: 'Mingė 99360, Lithuania',
+		lat: 55.3614149,
+		lon: 21.2826139
+	},
+	{
+		label: 'Moorhead, Clay County, Minnesota, United States',
+		detail: 'Moorhead, MN, USA',
+		lat: 46.8737648,
+		lon: -96.7678039
+	},
+	{
+		label: 'Muff, Northern Ireland',
+		detail: 'Muff, Co. Donegal, Ireland',
+		lat: 55.0680564,
+		lon: -7.2689498
+	},
+	{
+		label: 'Muff, Armstrong, Pennsylvania, United States',
+		detail: 'Muff, Wayne, PA 16222, USA',
+		lat: 40.8845093,
+		lon: -79.3380963
+	},
+	{
+		label: 'Muff, Donegal, Ireland',
+		detail: 'Muff, Co. Donegal, Ireland',
+		lat: 55.0680564,
+		lon: -7.2689498
+	},
+	{
+		label: 'Nobber, Meath, Ireland',
+		detail: 'Nobber, Co. Meath, Ireland',
+		lat: 53.8217066,
+		lon: -6.7479398
+	},
+	{
+		label: 'Nob End, Bolton, United Kingdom',
+		detail: 'Nob End, Fontwell Road (E-bound, Hail and ride), Little Lever BL3, UK',
+		lat: 53.55662,
+		lon: -2.37584
+	},
+	{
+		label: 'Pee, Margibi, Liberia',
+		detail: 'Pee, Liberia',
+		lat: 6.606744,
+		lon: -10.063861
+	},
+	{
+		label: 'Penistone, South Yorkshire, Great Britain',
+		detail: 'Penistone, South Yorkshire S36, UK',
+		lat: 53.526141,
+		lon: -1.6255329
+	},
+	{
+		label: 'Phuket, Phuket, Thailand',
+		detail: 'Phuket, Thailand',
+		lat: 7.9843109,
+		lon: 98.3307468
+	},
+	{
+		label: 'Pis, Asturias, Principado de Asturias, Spain',
+		detail: '33327 Pis, Asturias, Spain',
+		lat: 43.4784615,
+		lon: -5.3023733
+	},
+	{
+		label: 'Pis, Gers, Midi-Pyrénées, France',
+		detail: 'Pis, France',
+		lat: 43.784211,
+		lon: 0.709776
+	},
+	{
+		label: 'Poo, Asturias, Principado de Asturias, Spain',
+		detail: 'Póo, Asturias, Spain',
+		lat: 43.4247995,
+		lon: -4.7825354
+	},
+	{
+		label: 'Poo, Himachal, India',
+		detail: 'Poo, Himachal Pradesh, India',
+		lat: 31.755152,
+		lon: 78.603472
+	},
+	{
+		label: 'Probe, Utah (Salt Lake), Salt lake City',
+		detail: 'Probe, Sandy, UT 84094, USA',
+		lat: 40.5777261,
+		lon: -111.8599339
+	},
+	{
+		label: 'Pussy, Savoie, Rhône-Alpes, France',
+		detail: 'Pussy, 73260 La Léchère, France',
+		lat: 45.5515789,
+		lon: 6.454918
+	},
+	{
+		label: 'Reddick, Florida, United States',
+		detail: 'Reddick, FL 32686, USA',
+		lat: 29.3666393,
+		lon: -82.1973184
+	},
+	{
+		label: 'Rim, Guera, Chad',
+		detail: 'Rim, Chad',
+		lat: 10.6,
+		lon: 18.3299999
+	},
+	{
+		label: 'Rim, Nan, Thailand',
+		detail: 'Rim, Tha Wang Pha, Nan 55140, Thailand',
+		lat: 19.10107,
+		lon: 100.8022768
+	},
+	{
+		label: 'Ring, Ringville, Waterford, Ireland',
+		detail: 'Ring, Páirc An Bhóthair, Co. Waterford, Ireland',
+		lat: 52.0478899,
+		lon: -7.5824369
+	},
+	{
+		label: 'Ring, Winnebago, Wisconsin, United States',
+		detail: 'Ring, Nekimi, WI 54904, USA',
+		lat: 43.9091509,
+		lon: -88.6403889
+	},
+	{
+		label: 'Sac City, Iowa, United States',
+		detail: 'Sac City, IA 50583, USA',
+		lat: 42.4222033,
+		lon: -94.9897109
+	},
+	{
+		label: 'Semen, Jawa Timur, Indonesia',
+		detail: 'Semen 64161, Indonesia',
+		lat: -7.834983,
+		lon: 111.977158
+	},
+	{
+		label: 'Seymen, Turkey',
+		detail: 'Seymen/Tekirdağ Province, Turkey',
+		lat: 41.0935922,
+		lon: 27.9468675
+	},
+	{
+		label: 'Shaft, Pennsylvania, United States',
+		detail: 'Borden Shaft, MD 21532, USA',
+		lat: 39.630631,
+		lon: -78.929542
+	},
+	{
+		label: 'Shafter, California, United States',
+		detail: 'Shafter, CA, USA',
+		lat: 35.5005139,
+		lon: -119.2717761
+	},
+	{
+		label: 'Shag Island, Indiana, United States',
+		detail: 'Shag Island, Hamilton, IN 46742, USA',
+		lat: 41.5483838,
+		lon: -84.9216287
+	},
+	{
+		label: 'Shag Point, Otago, New Zealand',
+		detail: 'Shag Point, New Zealand',
+		lat: -45.460099,
+		lon: 170.8128119
+	},
+	{
+		label: 'Shit, Mazandaran, Iran',
+		detail: 'Shit, Iran',
+		lat: 36.480412,
+		lon: 53.557671
+	},
+	{
+		label: 'Slut, Vasterbotten, Sweden',
+		detail: 'Slut, Sweden',
+		lat: 64.733333,
+		lon: 20.516667
+	},
+	{
+		label: 'Spear, Avery, North Carolina, United States',
+		detail: 'Spear, Plumtree, NC 28657, USA',
+		lat: 36.022347,
+		lon: -82.0237284
+	},
+	{
+		label: 'Spear, Big Horn, Montana, United States',
+		detail: 'Spear, Montana 59050, USA',
+		lat: 45.1996881,
+		lon: -107.3792592
+	},
+	{
+		label: 'Spurt, Belgium',
+		detail: '9220 Spurt, Belgium',
+		lat: 51.08308,
+		lon: 4.13864
+	},
+	{
+		label: 'Stiff, Bretagne (Finistere, Ouessant), France',
+		detail: 'Le Stiff, 29242 Ushant, France',
+		lat: 48.471657,
+		lon: -5.060863
+	},
+	{
+		label: 'Swallow, Salt Lake, Utah, United States',
+		detail: 'Swallow, Salt Lake City, UT 84120, USA',
+		lat: 40.6935575,
+		lon: -112.0032724
+	},
+	{
+		label: 'Tampon, Reunion, Reunion',
+		detail: 'Le Tampon, Reunion',
+		lat: -21.286278,
+		lon: 55.519603
+	},
+	{
+		label: 'Three Cocks, Wales, United Kingdom',
+		detail: 'Three Cocks, Powys LD3, UK',
+		lat: 52.031866,
+		lon: -3.206616
+	},
+	{
+		label: 'Tit, Adrar, Algeria',
+		detail: 'Tit, Algeria',
+		lat: 26.936237,
+		lon: 1.4869856
+	},
+	{
+		label: 'Tit, Tamanghasset, Algeria',
+		detail: 'Tit, Algeria',
+		lat: 22.975901,
+		lon: 5.181231
+	},
+	{
+		label: 'Tittybong, Australia',
+		detail: 'Tittybong VIC 3542, Australia',
+		lat: -35.746196,
+		lon: 143.3753021
+	},
+	{
+		label: 'Twatt, Orkney, Scotland, United Kingdom',
+		detail: 'Twatt, Orkney, Orkney Islands KW17, UK',
+		lat: 59.098554,
+		lon: -3.274187
+	},
+	{
+		label: 'Twatt, Shetland, Scotland, United Kingdom',
+		detail: 'Twatt, Shetland Islands ZE2, UK',
+		lat: 60.265968,
+		lon: -1.409475
+	},
+	{
+		label: 'Vagina, Tyumenskaya oblast	, Russia',
+		detail: 'Vagina, Tyumen Oblast, Russia',
+		lat: 56.738527,
+		lon: 68.608464
+	},
+	{
+		label: 'Virgin Arm, Newfoundland, Canada',
+		detail: 'Virgin Arm-Carter\'s Cove, NL A0G, Canada',
+		lat: 49.536505,
+		lon: -54.781188
+	},
+	{
+		label: 'Virginville, Pennsylvania, United States',
+		detail: 'Virginville, Richmond, PA 19526, USA',
+		lat: 40.5239827,
+		lon: -75.8729805
+	},
+	{
+		label: 'Wank, Garmisch-Partenkirchen, Germany',
+		detail: 'Wank, 82467 Garmisch-Partenkirchen, Germany',
+		lat: 47.5166667,
+		lon: 11.15
+	},
+	{
+		label: 'Wank, Bayern (Ostallgäu, Nesselwang), Germany',
+		detail: 'Wank, 87484 Nesselwang, Germany',
+		lat: 47.6153139,
+		lon: 10.5213822
+	},
+	{
+		label: 'Wankie, Zimbabwe',
+		detail: 'Hwange, Zimbabwe',
+		lat: -18.368719,
+		lon: 26.501539
+	},
+	{
+		label: 'Wanks River, Nicaragua',
+		detail: 'Rio Coco',
+		lat: 14.9915534,
+		lon: -83.1955656
+	},
+	{
+		label: 'Wankum, Germany',
+		detail: 'Wankum, 47669 Wachtendonk, Germany',
+		lat: 51.4,
+		lon: 6.3
+	},
+	{
+		label: 'Wet Beaver Creek, Lake Montezuma, AZ, United States',
+		detail: 'Wet Beaver Creek, Lake Montezuma, AZ 86335, USA',
+		lat: 34.6328007,
+		lon: -111.8259849
+	},
+	{
+		label: 'Woody, Cumberland, Tennessee, United States',
+		detail: 'Woody, Tennessee 38571, USA',
+		lat: 36.0667339,
+		lon: -85.0638482
+	},
+	{
+		label: 'Woody, Greene, Illinois, United States',
+		detail: 'Woody, Bluffdale, IL 62027, USA',
+		lat: 39.2636584,
+		lon: -90.4851198
+	},
+	{
+		label: 'Woody, Kern, California, United States',
+		detail: 'Woody, CA 93287, USA',
+		lat: 35.7041199,
+		lon: -118.834259
+	}
+];
+
+window.onload = function() {
+	var place = Math.floor(Math.random() * places.length);
+	var coords = new L.LatLng(places[place].lat, places[place].lon);
+	var marker = new L.Marker(coords, {
+		title: places[place].label
+	});
+	var layer = new L.StamenTileLayer("toner");
+	var map = new L.Map("map", {
+		layers: layer
+	});
+	
+	var coords = [];
+	var markers = [];
+	var icon = new L.Icon({
+		iconUrl: '/images/signpost-icon.png',
+		iconSize: [32, 37]
+	});
+	for (var i=0; i < places.length; i++) {
+		var ll = new L.LatLng(places[i].lat, places[i].lon);
+		var mkr = new L.Marker(ll, {icon: icon});
+		var label = places[i].label;
+		mkr.bindPopup(label);
+		mkr.on('mouseover', function(e) {
+			e.target.openPopup();
+		});
+		coords.push(ll);
+		markers.push(mkr);
+		mkr.addTo(map);
+	}
+	
+	var bounds = new L.LatLngBounds(coords);
+	map.fitBounds(bounds);
+}
